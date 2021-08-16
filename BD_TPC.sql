0fb93c5a9f8f7b1298353bd1a90c0e4dda59b86d
Paulo Junior <pier.lon@hotmail.com>
2021-08-16 09:46:28 -0300

    Update BD_TPC.sql

--- BD_TPC.sql
+++ BD_TPC.sql
@@ -4,30 +4,20 @@ USE TPC;
 CREATE TABLE Db_Room (
 Name_Room VARCHAR(100),
 Code_Room INTEGER PRIMARY KEY,
-Amount_Players INTEGER,
 Started_Game BOOLEAN);
 
 CREATE TABLE Db_Basic (
 Id_Basic INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-Nick_Heritage VARCHAR(50),
-Question VARCHAR(200),
-Right_Answer VARCHAR(100),
-Wrong_Answer_One VARCHAR(100),
-Wrong_Answer_Two VARCHAR(100),
-Wrong_Answer_Three VARCHAR(100),
-Tip_One VARCHAR(100),
-Tip_Two VARCHAR(100),
-Tip_Three VARCHAR(100),
+Nick_Heritage VARCHAR(100),
+Question VARCHAR(400),
+Right_Answer VARCHAR(400),
+Wrong_Answer_One VARCHAR(400),
+Wrong_Answer_Two VARCHAR(400),
+Wrong_Answer_Three VARCHAR(400),
+Tip_One VARCHAR(400),
+Tip_Two VARCHAR(400),
+Tip_Three VARCHAR(400),
 Position INTEGER,
 foto LONGBLOB,
 Code_Room INTEGER,
 FOREIGN KEY (Code_Room) REFERENCES Db_Room(Code_Room));
-
-CREATE TABLE Db_Profile (
-Time_Player INTEGER,
-Nick_Player VARCHAR(12),
-Dice_Player INTEGER,
-Score_Player FLOAT,
-Map_Space INTEGER,
-Room INTEGER,
-Code_Room INTEGER);
